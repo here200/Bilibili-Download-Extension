d3cf14f19a5dee39269be11fbc3c80a5b565d021
ice036 <ice036@qq.com>
2022-11-21 09:24:58 +0800

    something small change

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from plus import ky_bilibili_plus
+from plus import bilibili_plus
 from common.download import ky_download
 from common.util import ky_debugs
 
 
 if __name__ == '__main__':
     # 第一种方法
-    all_songs = ky_bilibili_plus.get_all_audio_by_favorite("1598050455")
+    all_songs = bilibili_plus.get_all_audio_by_favorite("1598050455")
     ky_debugs.print_container_elements(all_songs)
     # for index in range(0, len(all_songs)):
     #     try:
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     #         continue
 
     # 第二种方法
-    # all_movies = ky_bilibili_plus.get_all_selections_by_one_movie("https://www.bilibili.com/video/BV1GE411R7sE")
+    # all_movies = bilibili_plus.get_all_selections_by_one_movie("https://www.bilibili.com/video/BV1GE411R7sE")
     # ky_debugs.print_container_elements(all_movies)
     # for index in range(0, len(all_movies)):
     #     try:
